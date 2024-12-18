091651ba5d43a3f4f8657af4a478cac77590fa6b
Muhammad Ayaan Qasmi <ayaan.qasmi@gmail.com>
2024-12-18 21:16:39 +0500

    Update authRoutes.js

--- UserAccMgmtServ/routes/authRoutes.js
+++ UserAccMgmtServ/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login } from "../controllers/authController.js"; // Adjust path as necessary
+import { login,validateToken  } from "../controllers/authController.js"; // Adjust path as necessary
 
 const router = express.Router();
 
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/login", login);
 
 // Route for validating a token (for protected routes)
-router.post("/validateToken");
+router.post("/validateToken",validateToken );
 
 export default router;
