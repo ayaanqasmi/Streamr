aaefc83859403b14557058dbb50bfca9f918284c
ayaanqasmi <ayaan.qasmi@gmail.com>
2024-12-29 13:29:41 +0500

    fixed build

--- ViewServ/src/app/edit/page.tsx
+++ ViewServ/src/app/edit/page.tsx
@@ -1,5 +1,5 @@
 import EditForm from "@/components/EditForm"
-
+import { UploadForm } from "@/components/UploadForm"
 export default function Page() {
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
